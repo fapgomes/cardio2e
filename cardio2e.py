afee6ceaaab43fc497018d5aa568f683c94e8c4b
Fernando A. P. Gomes <fapg@nsk.pt>
2025-02-01 18:21:50 +0000

    In the bypass zones, I don't have the response @I, so I process the answer in the ack.

--- cardio2e.py
+++ cardio2e.py
@@ -467,6 +467,19 @@ def listen_for_updates(serial_conn, mqtt_client):
                                 # Consultar o estado atual e publicar no MQTT
                                 #get_entity_state(serial_conn, mqtt_client, security_id, "S")
                                 _LOGGER.info("OK for action security: %s", security_id)
+                            elif message_parts[1] == "B":
+                                # Mensagem de estado das zonas, por exemplo: "@I B 1 NNNNNNNNNNNNNNNN"
+                                bypass_states = message_parts[3]
+
+                                # Processa cada caractere de estado para cada zona
+                                for zone_id in range(1, len(bypass_states) + 1):
+                                    bypass_state_char = bypass_states[zone_id - 1]  # Caractere correspondente à zona
+                                    bypass_state = cardio2e_zones.interpret_bypass_character(bypass_state_char)
+
+                                    # Publica o estado da zona no MQTT
+                                    state_topic = f"cardio2e/zone/bypass/state/{zone_id}"
+                                    mqtt_client.publish(state_topic, bypass_state, retain=False)
+                                    #_LOGGER.debug("Estado da zona %d publicado no MQTT: %s", zone_id, bypass_state)
                         elif len(message_parts) >= 3 and message_parts[0] == "@N":
                             error_msg = ""
                             if (message_parts[3] == "1"):
